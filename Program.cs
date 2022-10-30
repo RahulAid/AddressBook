47f01e72dbe7e6eae6cec49e94859ae35e5dc80b
RahulAid <raccess97@gmail.com>
2022-10-30 18:29:44 +0530

    Program to Add Contact Details

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace AddressBook
+﻿using AddressBook.Model;
+using AddressBook.Repository;
+
+namespace AddressBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+            ContactDetailsRepository contactDetails = new ContactDetailsRepository();
+            contactDetails.AddContactDetails("Rahul", "Kumar", 9999999999, "rk@gmail.com", "Yehlanka", "Banglore", "Karnataka", 560064);
+            contactDetails.DisplayContact();
+            Console.ReadLine();
         }
     }
 }
